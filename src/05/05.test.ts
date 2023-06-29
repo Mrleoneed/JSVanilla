344e0d12c1028a4b95e53dceb008dc5f4561f4bb
Mrleoneed <mrleoneed@gmail.com>
2023-06-29 20:41:42 +0300

    Less: 04 Filter

---
+++ src/05/05.test.ts
@@ -0,0 +1,21 @@
+import { ManType } from "./05";
+
+
+
+let people: Array<ManType> = []
+
+
+beforeEach(()=>{
+    people = [
+	{ name: 'Andrew Ivanov', age: 33 },
+	{ name: 'Alexander Petrov', age: 24 },
+	{ name: 'Dmitry Sidorov', age: 18 },
+];
+
+})
+
+test('greeting message', ()=>{
+    
+})
+
+
